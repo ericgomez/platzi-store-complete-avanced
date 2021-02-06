1bfb7fdeba189263690f7428ff4346d961fa98aa
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-02-06 12:42:53 -0600

    Enviendo Token con interceptores

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -2,10 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { TokenService } from './core/services/token.service';
+
+
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() { }
+  constructor(
+    private tokenService: TokenService
+  ) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     request = this.addToken(request);
@@ -13,7 +18,8 @@ export class AuthInterceptor implements HttpInterceptor {
   }
 
   private addToken(request: HttpRequest<any>) {
-    const token = '123';
+    const token = this.tokenService.getToken();
+    console.log(token, 'token');
     if(token) {
       request = request.clone({
         setHeaders: {
